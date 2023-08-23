341130927fa0af9e719e47e160239841586d1b43
NonymousMorlock <agamspaul@gmail.com>
2023-08-23 12:02:27 +0000

    injector

--- lib/core/services/injection_container.main.dart
+++ lib/core/services/injection_container.main.dart
@@ -1,22 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:education_app/src/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:education_app/src/auth/data/repos/auth_repo_impl.dart';
-import 'package:education_app/src/auth/domain/repos/auth_repo.dart';
-import 'package:education_app/src/auth/domain/usecases/forgot_password.dart';
-import 'package:education_app/src/auth/domain/usecases/sign_in.dart';
-import 'package:education_app/src/auth/domain/usecases/sign_up.dart';
-import 'package:education_app/src/auth/domain/usecases/update_user.dart';
-import 'package:education_app/src/auth/presentation/bloc/auth_bloc.dart';
-import 'package:education_app/src/on_boarding/data/datasources/on_boarding_local_data_source.dart';
-import 'package:education_app/src/on_boarding/data/repos/on_boarding_repo_impl.dart';
-import 'package:education_app/src/on_boarding/domain/repos/on_boarding_repo.dart';
-import 'package:education_app/src/on_boarding/domain/usecases/cache_first_timer.dart';
-import 'package:education_app/src/on_boarding/domain/usecases/check_if_user_is_first_timer.dart';
-import 'package:education_app/src/on_boarding/presentation/cubit/on_boarding_cubit.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:get_it/get_it.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+part of 'injection_container.dart';
 
 final sl = GetIt.instance;
 
@@ -29,7 +11,11 @@ Future<void> _initAuth() async {
   sl
     ..registerFactory(
       () => AuthBloc(
-          signIn: sl(), signUp: sl(), forgotPassword: sl(), updateUser: sl()),
+        signIn: sl(),
+        signUp: sl(),
+        forgotPassword: sl(),
+        updateUser: sl(),
+      ),
     )
     ..registerLazySingleton(() => SignIn(sl()))
     ..registerLazySingleton(() => SignUp(sl()))
