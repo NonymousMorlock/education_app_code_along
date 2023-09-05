b1772c61ce4d2aa0136078e3fa85c413b076fbaa
NonymousMorlock <agamspaul@gmail.com>
2023-09-05 13:50:56 +0000

    notification entity

--- lib/core/utils/typedefs.dart
+++ lib/core/utils/typedefs.dart
@@ -3,5 +3,4 @@ import 'package:dartz/dartz.dart';
 import 'package:education_app/core/errors/failures.dart';
 
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
-
 typedef DataMap = Map<String, dynamic>;
